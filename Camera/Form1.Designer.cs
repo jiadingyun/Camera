4af10432cf84105b2dbfaa9acc9ba117dcb65fb9
yaping <417777313@qq.com>
2018-07-18 18:46:50 +0800

    增加镜像

--- Camera/Form1.Designer.cs
+++ Camera/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnMirror = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn1.Location = new System.Drawing.Point(0, 64);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(150, 50);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "关闭摄像头";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 420);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 64);
+            this.listBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnMirror);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(401, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 420);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnMirror
+            // 
+            this.btnMirror.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMirror.Location = new System.Drawing.Point(0, 114);
+            this.btnMirror.Name = "btnMirror";
+            this.btnMirror.Size = new System.Drawing.Size(150, 49);
+            this.btnMirror.TabIndex = 4;
+            this.btnMirror.Text = "镜像关";
+            this.btnMirror.UseVisualStyleBackColor = true;
+            this.btnMirror.Click += new System.EventHandler(this.btnMirror_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(551, 420);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnMirror;
     }
 }
 
--- Camera/Form1.cs
+++ Camera/Form1.cs
@@ -5,16 +5,214 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace Camera
 {
     public partial class Form1 : Form
     {
+        //视频输入设备（摄像头）的集合
+        public FilterInfoCollection Cameras = null;
+
+        //本程序使用的那个摄像头
+        public VideoCaptureDevice Cam = null;
+
+        //摄像头信息
+        private FilterInfo filterInfo = null;
+        private bool isMirror;
+        /// <summary>
+        /// 镜像
+        /// </summary>
+        public bool IsMirror
+        {
+            get
+            {
+                return isMirror;
+            }
+
+            set
+            {
+                isMirror = value;
+            }
+        }
+
+        ///<summary>
+        ///窗体的构造函数：
+        /// Load事件用于在加载窗体时获取摄像头设备
+        /// FormClosed事件用于在直接关闭窗体时关闭摄像头，释放资源
+        ///</summary>
         public Form1()
         {
             InitializeComponent();
+            this.Load += Form1_Load;
+            this.FormClosed += Form1_FormClosed;
+        }
+
+        ///<summary>
+        ///窗体构造函数的Load事件处理程序
+        ///用于获取摄像头设备
+        ///</summary>
+        ///<paramname="sender"></param>
+        ///<paramname="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btn1.Enabled = false;
+            try
+            {
+                //1、获取并枚举所有摄像头设备
+                Cameras = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                //2、判断设备个数，选择某一设备
+                if (Cameras.Count > 0)
+                {
+                    listBox1.DataSource = Cameras;
+                    listBox1.DisplayMember = "Name";
+                    listBox1.ValueMember = "MonikerString";
+                    listBox1.SelectedValueChanged += ListBox1_SelectedValueChanged;
+
+                    btn1.Enabled = true;
+                    filterInfo = (FilterInfo)listBox1.SelectedItem;
+                    Cam = new VideoCaptureDevice(filterInfo.MonikerString);
+                    Cam.NewFrame += Cam_NewFrame;
+                    Cam.VideoSourceError += Cam_VideoSourceError;
+                    Cam.Start();
+                }
+                else
+                {
+                    MessageBox.Show("未找到视频输入设备！");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        /// <summary>
+        /// listBox选择项改变事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            //关闭老设备。
+            if (Cam != null)
+            {
+                Cam.SignalToStop();
+                //Cam.Stop();
+                btn1.Enabled = false;
+                btn1.Text = "关闭摄像头";
+                pictureBox1.Image = null;
+            }
+            //开始新设备。
+            btn1.Enabled = true;
+            filterInfo = (FilterInfo)listBox1.SelectedItem;
+            Cam = new VideoCaptureDevice(filterInfo.MonikerString);
+            Cam.NewFrame += Cam_NewFrame;
+            Cam.VideoSourceError += Cam_VideoSourceError;
+
+            Cam.Start();
+        }
+
+        /// <summary>
+        /// 错误事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void Cam_VideoSourceError(object sender, VideoSourceErrorEventArgs eventArgs)
+        {
+            if (eventArgs.Description != "正在中止线程。")//排除调用Stop()的错误提示！。
+            {
+                //MessageBox.Show(filterInfo.Name + " 错误：\n" + eventArgs.Description + " 请选择其他设备。");
+                BeginInvoke(new Action(() =>
+                {
+                    btn1.Enabled = false;
+                    pictureBox1.Image = null;
+                    Cam.SignalToStop();
+                    //Cam.Stop();//不能停止，所有用SigalToStop();
+                    MessageBox.Show("设备：\"" + filterInfo.Name + "\"无法使用，请选择其他设备。");
+                }));
+            }
+        }
+
+        ///<summary>
+        ///摄像头设备Cam的NewFrame事件处理程序
+        ///用于实时显示捕获的视频流
+        ///</summary>
+        ///<paramname="sender"></param>
+        ///<paramname="eventArgs"></param>
+        private void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (eventArgs.Frame.Clone() != null)
+            {
+                Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+                if (IsMirror)
+                {
+                    image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                }
+                pictureBox1.Image = image;
+            }
+            else
+            {
+                btn1.Enabled = false;
+                pictureBox1.Image = null;
+            }
+        }
+
+
+
+        ///<summary>
+        ///在关闭窗体的事件处理程序中，释放摄像头
+        ///</summary>
+        ///<paramname="sender"></param>
+        ///<paramname="e"></param>
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Cam != null && Cam.IsRunning)
+            {
+                Cam.Stop();
+            }
+        }
+
+        ///<summary>
+        ///点击按钮的事件处理程序
+        ///用于控制摄像头的开启、关闭
+        ///</summary>
+        ///<paramname="sender"></param>
+        ///<paramname="e"></param>
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (Cam.IsRunning)
+            {
+                Cam.Stop();
+                pictureBox1.Image = null;
+                btn1.Text = "开启摄像头";
+            }
+            else
+            {
+                Cam.Start();
+                btn1.Text = "关闭摄像头";
+            }
+        }
+
+        private void btnMirror_Click(object sender, EventArgs e)
+        {
+
+            IsMirror = IsMirror ? false : true;
+            if (IsMirror)
+            {
+                btnMirror.Text = "镜像关";
+            }
+            else
+            {
+                btnMirror.Text = "镜像开";
+            }
         }
     }
 }
